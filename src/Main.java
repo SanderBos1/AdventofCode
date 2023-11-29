7bf7853f5ee19cbabce500e062593287eb87ce51
Sander Bos <sanderb0ss@live.nl>
2023-11-29 11:41:47 +0100

    Solved Exercise 3_1

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 package src;
 
-import src.year_2022.day2.*;;
+import src.year_2022.day3.*;;
 
 public class Main {
 
     public static void main(String[] args) {
-        day2_2.Execute();
+        day3_1.Execute();
     }
 
 }
