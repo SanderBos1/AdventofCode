68bcd60936567cb106271d14cf37f2629dd4918c
Sander Bos <sanderb0ss@live.nl>
2024-01-09 11:15:04 +0100

    Finished day10_2

--- 2023/day10/day10_1.py
+++ 2023/day10/day10_1.py
@@ -13,7 +13,7 @@ def read_input():
     data = np.array(data)
     return data
 
-def queue_logic(data, visited, distance, q):
+def queue_logic(data, visited, distance, values, q):
     both = q.popleft()
     current = both[0]
     current_value = both[1]
@@ -30,27 +30,30 @@ def queue_logic(data, visited, distance, q):
                 q.append([down, data[down[0], down[1]]])
                 visited[down[0], down[1]] = True
                 distance[down[0], down[1]] = distance[current[0], current[1]] +1
-
+                values[down[0], down[1]] = current_value
         if current_value in ["J", "L", "|"]:
             if visited[up[0], up[1]] == False:
                 q.append([up, data[up[0], up[1]]])
                 visited[up[0], up[1]] = True
                 distance[up[0], up[1]] = distance[current[0], current[1]] +1
+                values[up[0], up[1]] = current_value
 
         if current_value in ["7", "J" ,"-"]:
             if visited[left[0], left[1]] == False:
                 q.append([left, data[left[0], left[1]]])
                 visited[left[0], left[1]] = True
                 distance[left[0], left[1]] = distance[current[0], current[1]] +1
+                values[left[0], left[1]] = current_value
 
         if current_value in ["L", "F", "-"]:
             if visited[right[0], right[1]] == False:
                 q.append([right, data[right[0], right[1]]])
                 visited[right[0], right[1]] = True
                 distance[right[0], right[1]] = distance[current[0], current[1]] +1
+                values[right[0], right[1]] = current_value
 
   
-    return data, visited, distance, q
+    return data, visited, distance,values,  q
 
 def find_value_S(s_position, data):
         
@@ -82,21 +85,33 @@ def find_value_S(s_position, data):
         else:
             return "|"
         
+                
+
+        
 def Main():
     data = read_input()
     row = len(data)
     column = len(data[0])
     s_position = [x[0] for x in  np.where(data=="S")]
+
     visited = np.full((row, column), False)
     distance = np.matrix(np.zeros((row, column)))
+    values = np.matrix(np.chararray((row, column)))
+    values[:] = "O"
+    s_value = find_value_S(s_position, data)
 
     visited[s_position[0], s_position[1]] = True
     distance[s_position[0], s_position[1]] = 0
+    values[s_position[0], s_position[1]] = s_value
+    
+    print(s_value)
 
     q = deque()
-    q.append([s_position, find_value_S(s_position, data)])
+    q.append([s_position, s_value])
     while(len(q) != 0):
-        data, visited, distance, q = queue_logic(data, visited, distance, q)
+        data, visited, distance, values, q = queue_logic(data, visited, distance, values, q)
+    print(values)
+
     return distance
 
 
--- 2023/day10/day10_2.py
+++ 2023/day10/day10_2.py
@@ -1,5 +1,5 @@
 import numpy as np
-from collections import deque
+import math
 
 def read_input():
     with open('2023/day10/input.txt') as file:
@@ -13,47 +13,6 @@ def read_input():
     data = np.array(data)
     return data
 
-def queue_logic(data, visited, distance, values, q):
-    both = q.popleft()
-    current = both[0]
-    current_value = both[1]
-
-    if current[0] >= 0 and current[0] < len(data) and current[1] >=0 and current[1] < len(data[0]):
-    
-        right = [current[0], current[1]+1]
-        left = [current[0], current[1]-1]
-        down = [current[0]+1, current[1]]
-        up = [current[0]-1, current[1]]
-        
-        if current_value in ["F", "7" ,"|"]:
-            if visited[down[0], down[1]] == False:
-                q.append([down, data[down[0], down[1]]])
-                visited[down[0], down[1]] = True
-                distance[down[0], down[1]] = distance[current[0], current[1]] +1
-                values[down[0], down[1]] = current_value
-        if current_value in ["J", "L", "|"]:
-            if visited[up[0], up[1]] == False:
-                q.append([up, data[up[0], up[1]]])
-                visited[up[0], up[1]] = True
-                distance[up[0], up[1]] = distance[current[0], current[1]] +1
-                values[up[0], up[1]] = current_value
-
-        if current_value in ["7", "J" ,"-"]:
-            if visited[left[0], left[1]] == False:
-                q.append([left, data[left[0], left[1]]])
-                visited[left[0], left[1]] = True
-                distance[left[0], left[1]] = distance[current[0], current[1]] +1
-                values[left[0], left[1]] = current_value
-
-        if current_value in ["L", "F", "-"]:
-            if visited[right[0], right[1]] == False:
-                q.append([right, data[right[0], right[1]]])
-                visited[right[0], right[1]] = True
-                distance[right[0], right[1]] = distance[current[0], current[1]] +1
-                values[right[0], right[1]] = current_value
-
-  
-    return data, visited, distance,values,  q
 
 def find_value_S(s_position, data):
         
@@ -85,34 +44,57 @@ def find_value_S(s_position, data):
         else:
             return "|"
         
-                
-
+def shoelace(list_vertices):
+    holder = 0
+    for pair in range(len(list_vertices)-1):
+        holder = holder + list_vertices[pair][0] * list_vertices[pair+1][1] - list_vertices[pair][1] * list_vertices[pair+1][0]
+    holder = holder + list_vertices[-1][0] * list_vertices[0][1] - list_vertices[-1][1] * list_vertices[0][0]
+    return abs(holder)/2
         
 def Main():
     data = read_input()
-    row = len(data)
-    column = len(data[0])
     s_position = [x[0] for x in  np.where(data=="S")]
+    bends = ["F", "J", "L", "7"]
 
-    visited = np.full((row, column), False)
-    distance = np.matrix(np.zeros((row, column)))
-    values = np.matrix(np.chararray((row, column)))
-    values[:] = "O"
-    s_value = find_value_S(s_position, data)
+    pipes = {
+        "|": {(1, 0): (1, 0), (-1, 0): (-1, 0)},
+        "-": {(0, 1): (0, 1), (0, -1): (0, -1)},
+        "L": {(1, 0): (0, 1), (0, -1): (-1, 0)},
+        "J": {(0, 1): (-1, 0), (1, 0): (0, -1)},
+        "F": {(-1, 0): (0, 1), (0, -1): (1, 0)},
+        "7": {(0, 1): (1, 0), (-1, 0): (0, -1)},
+    }
 
-    visited[s_position[0], s_position[1]] = True
-    distance[s_position[0], s_position[1]] = 0
-    values[s_position[0], s_position[1]] = s_value
-    
-    print(s_value)
+    borders = 1
+    V = [s_position]
+    stepx, stepy = 0, 1
 
-    q = deque()
-    q.append([s_position, s_value])
-    while(len(q) != 0):
-        data, visited, distance, values, q = queue_logic(data, visited, distance, values, q)
-    print(values)
+    x = s_position[0] 
+    y = s_position[1]
 
-    return distance
+    x = x + stepx
+    y = y + stepy
+    step = 1
+    value = data[x, y]
+
+    while(value != "S"):
 
 
-print("The final answer is : ", int(np.max(Main())))
+        pipe = pipes[value]
+        stepx, stepy = pipe[stepx, stepy]
+
+        x = x + stepx
+        y = y + stepy
+        value = data[x, y]
+        if value in bends:
+            borders += 1
+            V.append([x, y])
+        step+=1
+
+    Area = math.floor(shoelace(V))
+    Enclosed = Area - step/2 +1
+    return Enclosed
+
+# vertices = [(1,6), (3, 1), (7, 2), (4, 4), (8, 5)]
+# print(shoelace(vertices))
+print("The final answer is : ", Main())
